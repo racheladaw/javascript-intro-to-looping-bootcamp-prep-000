b75cd7806009a7c2e0350fc4a2884d943e29c847
Rachel Williams <williamsrachelada@gmail.com>
2019-01-24 05:43:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,11 +15,13 @@ function whileLoop(num) {
   return "done"
 }
 
+function incrementVariable() {
+  i = i + 1;
+}
 
 function doWhileLoop(array) {
   do {array.pop()
-  } 
-  while {array.length > 0 && incrementVariable() 
   }
-}
+  while (array.length > 0 && incrementVariable()) 
+  return array
 }
